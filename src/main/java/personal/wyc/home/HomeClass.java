5088b9c25f855d8db2538f42133fb4fbddf14814
kenyon <269097088@qq.com>
2022-10-02 18:50:48 +0800

    项目工程初始化

---
+++ src/main/java/personal/wyc/home/HomeClass.java
@@ -0,0 +1,13 @@
+package personal.wyc.home;
+
+/**
+ * @ClassName HomeClass
+ * @Description TODO
+ * @Author Kenyon Wong
+ * @Date 2022/10/2 0002 18:42
+ */
+public class HomeClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
